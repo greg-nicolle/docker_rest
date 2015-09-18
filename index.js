a5716eb40f722cb01f2637014941d878811e9af8
greg-nicolle <greg.nicolle@gmail.com>
2015-09-18 22:56:27 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,24 @@
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 8080 });
+
+server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+                    reply('Hello, world!');
+                        }
+});
+
+server.route({
+        method: 'GET',
+        path: '/{name}',
+        handler: function (request, reply) {
+                    reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+                        }
+});
+
+server.start(function () {
+        console.log('Server running at:', server.info.uri);
+});
